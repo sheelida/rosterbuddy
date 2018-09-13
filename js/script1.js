258dcbee3802e6a4e0689f405dcd79183984b009
Shelida Gomes <6207@ait.nsw.edu.au>
2018-09-13 06:24:25 +0000

    yani

--- js/script1.js
+++ js/script1.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function() {
         }
         if(password == '') {
             $(this).find('.error').fadeOut('fast', function(){
-                $(this).css('top', '96px');
+                $(this).css('top', '20px');
             });
             $(this).find('.error').fadeIn('fast', function(){
                 $(this).parent().find('.password').focus();
